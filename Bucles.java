45c7448d3a8ebfe7e1afeff2aa9e5a9a46863d2b
am291092 <am291092@gmail.com>
2023-05-02 10:27:53 +0200

    3funcionalidad

---
+++ Bucles.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Bucles {
+static Scanner sc=new Scanner(System.in);
+	public static void main(String[] args) {
+		System.out.println("Hasta que numero?");
+		int num=sc.nextInt();
+		for(int i=0;i<num;i++) {
+			
+			System.out.print(i+", ");
+			
+			
+		}
+		
+		System.out.println("Pares:");
+		for(int i=0;i<num;i++) {
+	
+	if(i%2==0) {
+		System.out.println(i+", ");
+	}
+			
+		
+
+	}
+
+}
